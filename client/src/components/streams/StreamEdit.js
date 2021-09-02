220fef7596633e5554cddfa5d52cbf3e2b9ffea7
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2021-09-02 19:29:12 +0530

    stream edit page basic logic added

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-  return (
-    <>
-      StreamEdit
-    </>
-  )
+// const StreamEdit = ({ history, location, match, stream }) => {
+//   console.log(stream)
+//   return <>StreamEdit</>;
+// };
+
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    console.log(this.props);
+    if (!this.props.stream) {
+      return <>Loading...</>;
+    }
+    return <>{this.props.stream.title}</>;
+  }
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
